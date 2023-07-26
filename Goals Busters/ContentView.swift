d6fd80485f4dd33f11a6985acc354407b47ce570
daniellelewis14 <yurts.third_0v@icloud.com>
2023-07-26 12:05:17 -0400

    Initial Commit

---
+++ Goals Busters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Goals Busters
+//
+//  Created by Danielle Lewis on 7/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Goals Busters/Goals_BustersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Goals_BustersApp.swift
+//  Goals Busters
+//
+//  Created by Danielle Lewis on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Goals_BustersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
