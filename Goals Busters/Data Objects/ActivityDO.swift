ec8427849a6fc8a17bc702606571f793bf6c0250
daniellelewis14 <yurts.third_0v@icloud.com>
2023-08-10 13:58:31 -0400

    Add sampleData, initializer, and goalCompletions property

--- Goals Busters/Data Objects/ActivityDO.swift
+++ Goals Busters/Data Objects/ActivityDO.swift
@@ -10,6 +10,24 @@ struct ActivityDO: Identifiable, Codable, Equatable {
     var id = UUID()
     var name: String
     var description: String
-    var completionCount = 0
+    var completionCount: Int
+    var goalCompletions: Int
     var color = Color.blue
+    
+    init(id: UUID = UUID(), name: String, description: String, completionCount: Int = 0, goalCompletions: Int, color: SwiftUI.Color = Color.blue) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.completionCount = completionCount
+        self.goalCompletions = goalCompletions
+        self.color = color
+    }
+}
+
+extension ActivityDO {
+    static let sampleData = [
+        ActivityDO(name: "Learn VisionOS", description: "Get the VisionOS SDK and get to work!", goalCompletions: 100),
+        ActivityDO(name: "Learn SwiftUI", description: "100 Days of SwiftUI", goalCompletions: 100),
+        ActivityDO(name: "Exercise", description: "100 push ups a day for 30 days", goalCompletions: 30)
+    ]
 }
