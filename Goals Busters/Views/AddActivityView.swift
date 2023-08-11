db613ba3ee328b9b643ad8587af3bc37ba072163
daniellelewis14 <yurts.third_0v@icloud.com>
2023-08-11 17:13:14 -0400

    Restore ColorPicker

--- Goals Busters/Views/AddActivityView.swift
+++ Goals Busters/Views/AddActivityView.swift
@@ -12,70 +12,81 @@ struct AddActivityView: View {
     @State private var description = ""
     @State private var goalCompletions = 0
     @ObservedObject var oo: ActivityOO
-    @State private var color = Color.black
+    @State private var color = Color.blue
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        Spacer()
         
-            VStack(alignment: .leading, spacing: 20) {
-                Text("Add New Goal")
-                    .font(.system(size: 30, design: .serif).weight(.bold))
-                    .padding([.bottom, .leading])
-                HStack {
-                    Text("Name")
+        Spacer()
+        ZStack {
+            
+            TransparentBlurView()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Add New Goal")
+                        .font(.system(size: 30, design: .serif).weight(.bold))
+                        .padding([.bottom, .leading])
+                    HStack {
+                        Text("Name")
+                            .font(.system(.headline, design: .serif))
+                            .padding(5)
+                        Spacer()
+                        ColorPicker("", selection: $color)
+                    }
+                    TextField("\(name)", text: $name)
+                        .padding(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(color, lineWidth: 2)
+                        )
+                        .padding(5)
+                    Text("Description")
                         .font(.system(.headline, design: .serif))
                         .padding(5)
-                    Spacer()
-                    ColorPicker("", selection: $color)
-                }
-                TextField("\(name)", text: $name)
-                    .padding(10)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(color, lineWidth: 2)
-                    )
-                    .padding(5)
-                Text("Description")
-                    .font(.system(.headline, design: .serif))
-                    .padding(5)
-                TextField(("\(description)"), text: $description)
-                    .padding(10)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(color, lineWidth: 2)
-                    )
-                    .padding(5)
-                HStack {
-                    Text("Goal Completions: ") +
-                    Text(" \(goalCompletions + 1)")
-                }
-                    .font(.system(.headline, design: .serif))
-                    .padding(5)
-                Picker("Goal completions", selection: $goalCompletions) {
-                    ForEach(1..<201) { number in
-                        Text("\(number)")
+                    TextField(("\(description)"), text: $description)
+                        .padding(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(color, lineWidth: 2)
+                        )
+                        .padding(5)
+                    HStack {
+                        Text("Desired Activity Completions: ") +
+                        Text(" \(goalCompletions + 1)")
                     }
+                    .font(.system(.headline, design: .serif))
+                    .padding(5)
+                    Picker("Goal completions", selection: $goalCompletions) {
+                        ForEach(1..<201) { number in
+                            Text("\(number)")
+                        }
+                    }
+                    .pickerStyle(.wheel)
                 }
-                .pickerStyle(.wheel)
+                
+                Spacer()
+                
+                Button {
+                    let goal = ActivityDO(name: name, description: description, goalCompletions: goalCompletions, color: color)
+                    oo.data.append(goal)
+                    dismiss()
+                } label: {
+                    Text("Save")
+                        .padding(12)
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .background(color)
+                        .cornerRadius(8)
+                }
+                .shadow(color: color, radius: 2, y: 5)
+                .disabled(name.isEmpty || description.isEmpty)
             }
             
-            Spacer()
             
-            Button {
-                let goal = ActivityDO(name: name, description: description, goalCompletions: goalCompletions, color: color)
-                oo.data.append(goal)
-                dismiss()
-            } label: {
-                Text("Save")
-                    .padding(12)
-                    .foregroundColor(.white)
-                    .fontWeight(.bold)
-                    .background(color)
-                    .cornerRadius(8)
-            }
-            .shadow(color: color, radius: 20, y: 5)
-        
+        }
     }
 }
 
